82e40cd20e6cd4b27474692588a63e4bc15ee355
Clint Berry <clint@system-11.com>
2013-09-17 16:11:59 -0600

    Added getProfiles and getStatus. Parsing plain text garbage from sofia. Lame.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 var esl = require('modesl');
 var _ = require('lodash');
-
+var csv = require('csv');
 
 
 function Connection(address, port, password) {
@@ -54,7 +54,7 @@ Connection.prototype.subscribe = function(events, cb) {
   if (typeof events == 'string' || events instanceof String) {
     this.eventArray.push(events);
   }
-  else if (typeof(events) == 'Array') {
+  else if (_.isArray(events)) {
     this.eventArray = _.union(this.eventArray, events);
   }
   else {
@@ -75,7 +75,7 @@ Connection.prototype.unsubscribe = function(events, cb) {
   else if (typeof(events) == 'Array') {
     _.forEach(events, function(value) {
       this.eventArray = _.without(this.eventArray, value);
-    })
+    });
   }
   else {
     cb('First argument must be event name string, or array of event names');
@@ -84,14 +84,32 @@ Connection.prototype.unsubscribe = function(events, cb) {
   // No unsubscribe in modesl library just yet. Need to submit a pull request
 }
 
+/**
+ * Event handler for freeswitch events
+ * @param  {String} eventName   Freeswitch event name (e.g. CHANNEL_CREATE, CUSTOME, etc.)
+ * @param  {Function} handler   Function to call when event fires
+ * @return {Null}               Null
+ */
 Connection.prototype.on = function(eventName, handler) {
   this.conn.on('esl::event::' + eventName + '::**', function(e) {
     handler(e);
   });
 }
 
+/**
+ * Advand On event handler for direct access to node-esl event handler
+ * @param  {String} eventName  Full event namespace including esl::event:**
+ * @param  {Function} handler  Function to call when event fires
+ * @return {Null} Null
+ */
+Connection.prototype.advOn = function(eventName, handler) {
+  this.conn.on(eventName, function(e) {
+    handler(e);
+  });
+}
+
 /*************************************************
-  Channels
+  Channel Commands
 **************************************************/
 
 Connection.prototype.getChannels = function(cb) {
@@ -130,7 +148,98 @@ Connection.prototype.getCalls = function(cb) {
   }
 }
 
+/*************************************************
+  Sofia Commands
+**************************************************/
 
+Connection.prototype.status = function(cb) {
+  var instance = this;
+  sendApiRequest(instance, 'status', function(data) {
+    cb(null, data);
+  });
+}
+
+Connection.prototype.getProfiles = function(cb) {
+  var instance = this;
+  var names = [];
+  var parsedData = [];
+  var parse = function(data) {
+    csv().from.string(data, { comment:'=', delimiter: '\t', columns: true, trim: true }).to.array(function(parsedData){
+      parsedData.pop();
+      cb(null, parsedData);
+    });
+  }
+  sendApiRequest(instance, 'sofia status', parse);
+}
+
+
+Connection.prototype.getUsers = function(profile, cb) {
+  var instance = this;
+  var users = [];
+
+  // If callback is first argument, get all profiles and check for devices accross all of them
+  if(_.isFunction(profile)) {
+    cb = profile;
+    instance.getProfiles(function(err, profiles) {
+      var splicedProfiles = [];
+      for(var i=0; i<profiles.length; i++) {
+        if(profiles[i]['Type'] == 'profile') {
+          splicedProfiles.push(profiles[i]);
+        }
+      };
+      var count=0
+      splicedProfiles.forEach(function(profile, index) {
+        count++;
+        sendApiRequest(instance, 'sofia status profile ' + profile.Name + ' reg', function(data){
+          csv().from.string(data, { comment:'==', delimiter: '\t', trim: true, quote: '' }).to.array(function(parsedData){
+            parsedData.shift(); // Remove first non-data element
+            parsedData.pop(); // Remove blank line
+            parsedData.pop(); // Remove the summary at end
+            var tempUsers = [];
+            var currentUser = {};
+            parsedData.forEach(function(namevalue, idx) {
+              if(namevalue[0] == 'Call-ID:') {
+                if('Call-ID' in currentUser) {
+                  tempUsers.push(_.clone(currentUser)); // pushing objects pushes just the reference. Need to clone.
+                }
+                currentUser = {};
+              }
+              currentUser[namevalue[0].replace(/:/, "")] = namevalue[1];
+            });
+            
+            // Add the last current user to the list
+            if('Call-ID' in currentUser) {
+              tempUsers.push(_.clone(currentUser));
+            }
+            users = users.concat(tempUsers);
+            count--;
+            if(count == 0) {
+              cb(null, users);
+            }
+          });
+        });
+      });
+    });
+  }
+}
+
+function sendApiRequest(instance, apiCommand, cb) {
+  var tempFunction = function(callback) {
+    instance.conn.bgapi(apiCommand, function(res) {
+      //console.log(res.getBody());
+      callback(res.getBody());
+    });
+  }
+  if(instance.isConnected()) {
+    tempFunction(cb);
+  }
+  else {
+    instance.pendingFunctions.push({
+      func: tempFunction,
+      cb: cb
+    });
+  }
+}
 
 
 module.exports = {
