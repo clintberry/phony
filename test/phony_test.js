e07b52906d477a1a7a410482c3890044f51a6df0
Clint Berry <clint@system-11.com>
2013-12-03 17:35:35 -0700

    Added grunt tasks and prepped to start tests. Moved main file to lib/phony.js

---
+++ test/phony_test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var manager = require('../lib/phony.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['manager'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+    test.expect(1);
+    // tests here
+    manager.awesome = 'awesome';
+    test.equal(manager.awesome, 'awesome', 'should be awesome.');
+    test.done();
+  }
+};
